aefe632c759c6ff046f10597ddff4dda8766c55a
james baker <jkroner@berklee.edu>
2015-04-10 10:37:41 -0400

    Text - Scripts / Game Objects
    Doesn’t work yet, just adding them in to make sure they don’t make the
    branch unstable

---
+++ Angry Birds Project/Assets/Scripts/Score.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start () {
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText (); 
+		}
+		//Destroy(other.gameObject);
+		//gameObject.tag = "Player";
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "YOU WIN!!!";
+		}
+	}
+
+}
